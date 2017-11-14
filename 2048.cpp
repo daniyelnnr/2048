4dc11abe13f089b4d479036c057f5b8056eeb040
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2017-11-14 20:54:47 -0300

    Formatação do board.

--- 2048.cpp
+++ 2048.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -9,53 +10,23 @@ int board[4][4] = {
     {13,14,15,16}
 };
 
-/*void showValue(int row_no, int col_no) {
-     if (box_values[row_no][col_no] != 0) {  
-          printf("%5d", box_values[row_no][col_no]);  
-     }else{  
-          printf("   ");  
-     }  
-}  
-  
-void showRow(int row_no) {  
-     for (int col_no = 0; col_no < 4; col_no++)  
-     {  
-          printf("|");  
-          DisplayValue(row_no, col_no);  
-     }  
-     printf("|\n\n");  
-} */
-
 void buildBoard() {
   for (int row = 0; row < 4; row++) { 
+    cout << "+------+------+------+------+" << endl << "| ";    
     for (int col = 0; col < 4; col++) {
-      
+      if (board[row][col] =! 0) {
+        cout << setw(4) << board[row][col];
+        cout << " | ";
+      } else {
+        cout << setw(4) << " ";
+      }
     }
+    cout << endl;
   }
+  cout << "+------+------+------+------+" << endl << endl;
 }
 
-void DisplayBox(void) {   
-     printf("-----------------------------------\n\n");  
-     printf("w -> move up\n");  
-     printf("a -> move left\n");  
-     printf("s -> move down\n");  
-     printf("d -> move right\n");  
-     printf("x -> exit\n\n");  
-  
-     for (int row_no = 0; row_no < 4; row_no++)  
-     {  
-          printf("-------------------------\n\n");  
-          DisplayRow(row_no);  
-     }  
-     printf("-------------------------\n\n");  
-}  
-
 int main() {
-  cout << "HELLO";
-  //int box_values[4][4] = { 0 };
-  
-  //printf("%5d", box_values[0][0]);
-
-  DisplayBox();
+  buildBoard();
   return 0;
 }
