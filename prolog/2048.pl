ab8f5b6c4a2aeaa0c0111da747c81ec243acdab6
igormatheusdiniz <igor.pinheiro@ccc.ufcg.edu.br>
2018-03-05 16:43:45 -0300

    MUdança na logica de jogo, movimentos direita esquerda totalmente funcionais, sfits funcionais, leve bug movimento vertical

--- prolog/2048.pl
+++ prolog/2048.pl
@@ -1,4 +1,8 @@
-:- use_module(library(random)).
+:- initialization(main).	
+menu():-
+	printa(),
+	read(OP),
+	sw(OP).
 
 telaInicial() :-
     write( "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"),nl,
@@ -41,57 +45,237 @@ telaInicial() :-
     write( "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"),nl,
     write( ""                                                                             ),nl.
 
-print_matrix([]).
-print_matrix([H|T]) :- 
-    format('~w ~w ~w ~w~n', H),
-    print_matrix(T).
 
-start(G) :-
-    G = [
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0]
-    ].
+sw(1):-
+	findall(X, (array(X,_,_,_,_)),L),
+	direita(L),
+	menu().	
+sw(2):-
+	findall(X, (array(X,_,_,_,_)),L),
+	esquerda(L),
+	menu().	
+sw(3):-
+	array(0,X,Y,W,Z),
+	array(1,X1,Y1,W1,Z1),
+	array(2,X2,Y2,W2,Z2),
+	array(3,X3,Y3,W3,Z3),
+	
+	
+	assertz(array(4,X,X1,X2,X3)),
+	assertz(array(5,Y,Y1,Y2,Y3)),
+	assertz(array(6,W,W1,W2,W3)),
+	assertz(array(7,Z,Z1,Z2,Z3)),
+	
+	shif(4),
+	shif(5),
+	shif(6),
+	shif(7),
+	
+	
+	somaDireita(4),
+	somaDireita(5),
+	somaDireita(6),
+	somaDireita(7),
+	
+	
+	array(4,A1,A2,A3,A4),
+	array(5,B1,B2,B3,B4),
+	array(6,C1,C2,C3,C4),
+	array(7,D1,D2,D3,D4),
+	
+	retract(array(0,_,_,_,_)),
+	retract(array(1,_,_,_,_)),
+	retract(array(2,_,_,_,_)),
+	retract(array(3,_,_,_,_)),
+	
+	assertz(array(0,A1,B1,C1,D1)),
+	assertz(array(1,A2,B2,C2,D2)),
+	assertz(array(2,A3,B3,C3,D3)),
+	assertz(array(3,A4,B4,C4,D4)),
+	
+	retract(array(4,_,_,_,_)),
+	retract(array(5,_,_,_,_)),
+	retract(array(6,_,_,_,_)),
+	retract(array(7,_,_,_,_)),
+	
+	menu().
 
-adicionaIniciais(G,Grid) :- 
-    random_member(Val,[2,2,2,2,2,2,2,2,2,4]),
-    /*substituir Val em uma posicao aleatoria de G*/
-    length(G, Size),
-    list_index(Size,Indexes),
-    random_member(X,Indexes),
-    random_member(Y,Indexes),
-    replace(G,X,Y,Val,Ans),
-    Grid = Ans.
 
-replace([H|Tail],0,Y,Z,[R|Tail]) :-
-    /*encontrou a linha desejada, agora procura pela coluna*/
-    /*lista de listas, entao H e R sao ambas listas*/ 
-    replace_column(H,Y,Z,R).
-replace([H|Tail],X,Y,Z,[H|R]) :-
-    /*recursivamente ate encontrara a linha desejada*/
-    X > 0,
-    X1 is X-1,
-    replace(Tail,X1,Y,Z,R).
+sw(4):-
+	array(0,X,Y,W,Z),
+	array(1,X1,Y1,W1,Z1),
+	array(2,X2,Y2,W2,Z2),
+	array(3,X3,Y3,W3,Z3),
+	
+	
+	assertz(array(4,X,X1,X2,X3)),
+	assertz(array(5,Y,Y1,Y2,Y3)),
+	assertz(array(6,W,W1,W2,W3)),
+	assertz(array(7,Z,Z1,Z2,Z3)),
+	
+	shif1(4),
+	shif1(5),
+	shif1(6),
+	shif1(7),
+	
+	
+	somaEsquerda(4),
+	somaEsquerda(5),
+	somaEsquerda(6),
+	somaEsquerda(7),
+	
+	
+	array(4,A1,A2,A3,A4),
+	array(5,B1,B2,B3,B4),
+	array(6,C1,C2,C3,C4),
+	array(7,D1,D2,D3,D4),
+	
+	retract(array(0,_,_,_,_)),
+	retract(array(1,_,_,_,_)),
+	retract(array(2,_,_,_,_)),
+	retract(array(3,_,_,_,_)),
+	
+	assertz(array(0,A1,B1,C1,D1)),
+	assertz(array(1,A2,B2,C2,D2)),
+	assertz(array(2,A3,B3,C3,D3)),
+	assertz(array(3,A4,B4,C4,D4)),
+	
+	retract(array(4,_,_,_,_)),
+	retract(array(5,_,_,_,_)),
+	retract(array(6,_,_,_,_)),
+	retract(array(7,_,_,_,_)),
+	
+	menu().
 
-replace_column([_|Tail],0,Z,[Z|Tail]).
-/*a cabeca sera substituida, formando a resposta*/
-replace_column([C|Tail],Y,Z,[C|R]) :-
-    /*recursivamente ate encontrara a coluna desejada*/
-    Y > 0,
-    Y1 is Y-1,
-    replace_column(Tail,Y1,Z,R).
+sw(_):-
+	halt(0).
 
-/*gera uma lista dos indices da lista, iniciando com 0*/
-list_index(0, []) :- !.
-list_index(M, [M1|Ks]) :-
-    M1 is M - 1,
-    list_index(M1, Ks).
 
-:- initialization(main).
+printa():-
+	findall(X, (array(X,_,_,_,_)),L),
+	listarArray(L).
+esquerda([]).
+esquerda([Head|Tail]):-
+	shif1(Head),
+	somaEsquerda(Head),
+	esquerda(Tail).
+somaEsquerda(Numero):- W=Z,X=Y,
+	array(Numero,X,Y,W,Z),
+	SOMA is W + Z,
+	SOMA2 is X + Y,
+	retract(array(Numero,_,_,_,_)),
+	assertz(array(Numero,SOMA2,SOMA,0,0)).	
+somaEsquerda(Numero):- W=Z,
+	array(Numero,X,Y,W,Z),
+	SOMA is W + Z,
+	retract(array(Numero,_,_,_,_)),
+	assertz(array(Numero,X,Y,SOMA,0)).
+
+somaEsquerda(Numero):- X=Y,
+	array(Numero,X,Y,W,Z),
+	SOMA is X + Y,
+	retract(array(Numero,_,_,_,_)),
+	assertz(array(Numero,SOMA,W,Z,0)).
+somaEsquerda(Numero):- W=Y,
+	array(Numero,X,Y,W,Z),
+	SOMA is W + Y,
+	retract(array(Numero,_,_,_,_)),
+	assertz(array(Numero,X,SOMA,Z,0)).	
+
+
+
+
+direita([]).
+direita([Head|Tail]):-
+	shif(Head),
+	somaDireita(Head),
+	direita(Tail).
+
+somaDireita(Numero):- W=Z,X=Y,
+	array(Numero,X,Y,W,Z),
+	SOMA is W + Z,
+	SOMA2 is X + Y,
+	retract(array(Numero,_,_,_,_)),
+	assertz(array(Numero,0,0,SOMA2,SOMA)).	
+somaDireita(Numero):- W=Z,
+	array(Numero,X,Y,W,Z),
+	SOMA is W + Z,
+	retract(array(Numero,_,_,_,_)),
+	assertz(array(Numero,0,X,Y,SOMA)).
+
+somaDireita(Numero):- X=Y,
+	array(Numero,X,Y,W,Z),
+	SOMA is X + Y,
+	retract(array(Numero,_,_,_,_)),
+	assertz(array(Numero,0,SOMA,W,Z)).
+somaDireita(Numero):- W=Y,
+	array(Numero,X,Y,W,Z),
+	SOMA is W + Y,
+	retract(array(Numero,_,_,_,_)),
+	assertz(array(Numero,0,X,SOMA,Z)).	
+		
+inicia():-
+	assertz(array(0,0,0,2,2)),
+	assertz(array(1,0,0,4,4)),
+	assertz(array(2,2,2,0,4)),
+	assertz(array(3,2,2,4,4)).
+
+listarArray([]).
+listarArray([H|Tail]):-
+	array(H,X,Y,W,Z),
+	format('|~w~5+|~w~5+|~w~5+|~w~5+|  ~n', [X,Y,W,Z]),
+	listarArray(Tail).
+	
+/* SHIFT DIREITA */
+shif(Numero):- array(Numero,X,Y,W,Z), X \= 0, Y = 0, W = 0, Z=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,0,0,0,X)).
+shif(Numero):- array(Numero,X,Y,W,Z), X = 0, Y \= 0, W = 0, Z=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,0,0,0,Y)).
+shif(Numero):- array(Numero,X,Y,W,Z), X = 0, Y = 0, W \= 0, Z=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,0,0,0,W)).																				
+
+shif(Numero):- array(Numero,X,Y,W,Z), X \= 0, Y \= 0, W = 0, Z=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,0,0,X,Y)).
+shif(Numero):- array(Numero,X,Y,W,Z), X \= 0, Y = 0, W \= 0, Z=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,0,0,X,W)).						
+shif(Numero):- array(Numero,X,Y,W,Z), X = 0, Y \= 0, W \= 0, Z=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,0,0,Y,W)).											
+
+shif(Numero):- array(Numero,X,Y,W,Z), X \= 0, Y \= 0, W \= 0, Z=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,0,X,Y,W)).
+shif(Numero):- array(Numero,X,Y,W,Z), X \= 0, Y \= 0, W = 0, Z\=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,0,X,Y,Z)).
+shif(Numero):- array(Numero,X,Y,W,Z), X \= 0, Y = 0, W \= 0, Z\=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,0,X,W,Z)).										
+
+/* SHIFT ESQUERDA */
+
+shif1(Numero):- array(Numero,X,Y,W,Z), X = 0, Y = 0, W = 0, Z\=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,Z,0,0,0)).
+shif1(Numero):- array(Numero,X,Y,W,Z), X = 0, Y \= 0, W = 0, Z=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,Y,0,0,0)).
+shif1(Numero):- array(Numero,X,Y,W,Z), X = 0, Y = 0, W \= 0, Z=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,W,0,0,0)).																				
+
+shif1(Numero):- array(Numero,X,Y,W,Z), X = 0, Y \= 0, W \= 0, Z=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,Y,W,0,0)).
+shif1(Numero):- array(Numero,X,Y,W,Z), X = 0, Y \= 0, W = 0, Z\=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,Y,Z,0,0)).						
+shif1(Numero):- array(Numero,X,Y,W,Z), X = 0, Y = 0, W \= 0, Z\=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,W,Z,0,0)).											
+
+shif1(Numero):- array(Numero,X,Y,W,Z), X = 0, Y \= 0, W \= 0, Z\=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,Y,W,Z,0)).
+shif1(Numero):- array(Numero,X,Y,W,Z), X \= 0, Y \= 0, W = 0, Z\=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,X,Y,Z,0)).
+shif1(Numero):- array(Numero,X,Y,W,Z), X \= 0, Y = 0, W \= 0, Z\=0, retract(array(Numero,_,_,_,_)),
+										assertz(array(Numero,X,W,Z,0)).	
+
+shif1(_).
+shif(_).										
 main:-
-    telaInicial(),
-    start(G),
-    adicionaIniciais(G,Aux),
-    adicionaIniciais(Aux,Grid),
-    print_matrix(Grid),nl.
+	telaInicial(),
+	inicia(),
+	menu(),
+	
+halt(0).
